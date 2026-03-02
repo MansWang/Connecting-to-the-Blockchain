ee7fe16a06af792f43fb06ff817e4413118abd7f
Sheila Wang <56094687+MansWang@users.noreply.github.com>
2026-03-01 23:41:57 -0500

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -1,5 +1,8 @@
-import json from web3 import Web3 from web3.middleware import
-ExtraDataToPOAMiddleware from web3.providers.rpc import HTTPProvider
+import json
+from web3 import Web3
+from web3.middleware import ExtraDataToPOAMiddleware
+from web3.providers.rpc import HTTPProvider
+
 
 '''
 If you use one of the suggested infrastructure providers, the url will be of the form
